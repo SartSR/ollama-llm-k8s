866f6c2dd9afcfb6b1c61f3011ba411e16e3013c
SartSR <126160078+SartSR@users.noreply.github.com>
2024-08-31 14:15:52 +0200

    Update gke.tf

--- gke.tf
+++ gke.tf
@@ -1,11 +1,11 @@
 module "gke" {
-  source = "terraform-google-modules/kubernetes-engine/google"
-  project_id = var.GCP_project
-  name       = "ollama-cluster"
-  region     = var.region
-  zones      = var.zones
-  network    = var.network
-
+  source      = "terraform-google-modules/kubernetes-engine/google"
+  project_id  = var.GCP_project
+  name        = "ollama-cluster"
+  region      = var.region
+  zones       = var.zones
+  network     = var.network
+  
   node_pools = [
     {
       name               = "default-pool"
@@ -15,20 +15,4 @@ module "gke" {
       initial_node_count = 1
     },
   ]
-}
-
-resource "google_container_cluster" "primary" {
-  name               = "ollama-cluster"
-  location           = var.region
-  initial_node_count = 1
-}
-
-resource "google_container_node_pool" "primary_nodes" {
-  cluster    = google_container_cluster.primary.name
-  location   = google_container_cluster.primary.location
-  node_count = 1
-
-  node_config {
-    machine_type = "e2-medium"
-  }
 }
