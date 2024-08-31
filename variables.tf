422b82caa0596897e583bd58bbaac9efe7077d95
SartSR <126160078+SartSR@users.noreply.github.com>
2024-08-31 13:39:59 +0200

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,38 @@
+variable "GCP_project" {
+  description = "The GCP project to deploy the infrastructure into."
+  type        = string
+}
+
+variable "region" {
+  description = "The GCP region to deploy the infrastructure into."
+  type        = string
+  default     = "us-central1"
+}
+
+variable "zones" {
+  description = "The GCP zones to deploy the infrastructure into."
+  type        = list(string)
+  default     = ["us-central1-a", "us-central1-b", "us-central1-c"]
+}
+
+variable "network" {
+  description = "The VPC network where the GKE cluster will be deployed."
+  type        = string
+  default     = "default"
+}
+
+variable "GITHUB_TOKEN" {
+  description = "GitHub Token to authenticate and create the Flux config repository."
+  type        = string
+}
+
+variable "github_owner" {
+  description = "The owner of the GitHub repository where Flux will be bootstrapped."
+  type        = string
+}
+
+variable "namespace" {
+  description = "The Kubernetes namespace for deploying Ollama and OpenWebUI."
+  type        = string
+  default     = "ollama-openwebui"
+}
